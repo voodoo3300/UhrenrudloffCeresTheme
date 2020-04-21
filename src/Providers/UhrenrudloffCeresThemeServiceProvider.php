e0228d0fe1c69a331cc5c47eda193290283881da
Voodoo3300 <rudloff.martin@googlemail.com>
2020-04-21 19:50:21 +0200

    Update UhrenrudloffCeresThemeServiceProvider.php

--- src/Providers/UhrenrudloffCeresThemeServiceProvider.php
+++ src/Providers/UhrenrudloffCeresThemeServiceProvider.php
@@ -9,7 +9,7 @@ use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
 use Plenty\Modules\Webshop\Template\Providers\TemplateServiceProvider;
- 
+
 //class UhrenrudloffCeresThemeServiceProvider extends ServiceProvider
 class UhrenrudloffCeresThemeServiceProvider extends TemplateServiceProvider
 {
